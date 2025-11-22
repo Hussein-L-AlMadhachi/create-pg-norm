7b711120aa36a8cc93c5e9de8f7bdf7eae6a0467
Hussein <almadhachihussein@gmail.com>
2025-11-22 12:37:42 +0300

    fixing version mismatch with PG-NORM

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ async function main() {
             "dev": "tsx"
         },
         "dependencies": {
-            "pg-norm": "^0.0.4"
+            "pg-norm": "^0.1.2"
         },
         "devDependencies": {
             "@types/node": "^24.9.1"
